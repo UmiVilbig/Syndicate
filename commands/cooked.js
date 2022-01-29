7b5aa8f3050f005351b4ff8a6d0309731c7afe75
Umi Vilbig <63989878+UmiVilbig@users.noreply.github.com>
2022-01-29 13:44:51 -0500

    removed edit function... annoying to set up

--- commands/cooked.js
+++ commands/cooked.js
@@ -25,66 +25,67 @@ module.exports = {
             })
             return;
         }
-
         
-        UserProfits.findOne({userId: id}, (err, profits) => {
-            if (err){
-                interaction.reply("An error occured when handling your request")
-                return
-            }
-            if(!profits){
-                if(direction === '+'){
-                    profits = new UserProfits({
-                    user_id: id,
-                    total_profits: amount,
-                    last_entry: amount
-                    })
-                } else {
-                    if(today === profits.today_date){
+        if(direction != '+' && direction != '-'){
+            interaction.reply({
+                content: 'please enter a direction + or -',
+                ephemeral: true
+            })
+        }
+        else{
+
+            UserProfits.findOne({userId: id}, (err, profits) => {
+                if (err){
+                    interaction.reply("An error occured when handling your request")
+                    return
+                }
+                if(!profits){
+                    if(direction === '+'){
                         profits = new UserProfits({
                             user_id: id,
-                            total_profits: value,
-                            last_entry: profits.last_entry + amount,
-                        })
-                    } else{
-                        profits = new UserProfits({
-                        user_id: id,
-                        total_profits: value,
-                        last_entry: amount
-                    })
+                            total_profits: amount,
+                            last_entry: amount
+                        })}
+                        else {
+                            profits = new UserProfits({
+                                user_id: id,
+                                total_profits: value,
+                                last_entry: value
+                            })
+                        }
+                    } else {
+                        console.log(direction)
+                        totalUserProfits = profits.total_profits
+                        if(direction === '+'){
+                            totalUserProfits = totalUserProfits + amount
+                            profits.total_profits = totalUserProfits
+                            profits.last_entry = amount
+                        } else {
+                            totalUserProfits = totalUserProfits - amount
+                            profits.total_profits = totalUserProfits
+                            profits.last_entry = 0 - amount
+                        }
                     }
+                    
+                    profits.save(err => {
+                        if(err){
+                            console.log('there was an error committing to db')
+                        }
+                        const userCooksEmbed = new Discord.MessageEmbed()
+                        .setColor("#e11f95")
+                        .setAuthor({name: 'Syndicate', iconURL: 'https://cdn.discordapp.com/attachments/933204859728068608/933204908734312518/syndicate_logo.jpg'})
+                        .setTitle("**Syndicate Profit Tracker**")
+                        .addFields(
+                            {name: 'Your Profits', value: `${profits.total_profits} SOL`, inline: true},
+                            )
+                            .setThumbnail('https://cdn.discordapp.com/attachments/933204859728068608/933204908734312518/syndicate_logo.jpg')
+                            .setTimestamp();
+                            
+                            interaction.reply({
+                                embeds: [userCooksEmbed]
+                            })
+                        })
+                    })
                 }
-            } else {
-            totalUserProfits = profits.total_profits
-                if(direction === '+' || direction === '0'){
-                    totalUserProfits = totalUserProfits + amount
-                    profits.total_profits = totalUserProfits
-                    profits.last_entry = amount
-                } else {
-                    totalUserProfits = totalUserProfits - amount
-                    profits.total_profits = totalUserProfits
-                    profits.last_entry = 0 - amount
                 }
-            }
-
-            profits.save(err => {
-                if(err){
-                    console.log('there was an error committing to db')
-                }
-                const userCooksEmbed = new Discord.MessageEmbed()
-                    .setColor("#e11f95")
-                    .setAuthor({name: 'Syndicate', iconURL: 'https://cdn.discordapp.com/attachments/933204859728068608/933204908734312518/syndicate_logo.jpg'})
-                    .setTitle("**Syndicate Profit Tracker**")
-                    .addFields(
-                        {name: 'Your Profits', value: `${profits.total_profits} SOL`, inline: true},
-                    )
-                    .setThumbnail('https://cdn.discordapp.com/attachments/933204859728068608/933204908734312518/syndicate_logo.jpg')
-                    .setTimestamp();
-    
-                interaction.reply({
-                    embeds: [userCooksEmbed]
-                })
-            })
-        })
-    }
 }
